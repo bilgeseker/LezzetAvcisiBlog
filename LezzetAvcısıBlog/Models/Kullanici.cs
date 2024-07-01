dd48b8b65a010a7ce6e448cf5711832f8e0c233f
bilgeseker <bilgeseker752@gmail.com>
2024-07-01 14:39:13 +0300

    first

---
+++ LezzetAvcısıBlog/Models/Kullanici.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LezzetAvcısıBlog.Models
+{
+	public class Kullanici
+	{
+        
+		public int id { get; set; }
+		public string ad { get; set; }
+        public string soyad { get; set; }
+        public string eposta { get; set; }
+        public string sifre { get; set; }
+        public int? tarif_id { get; set; }
+    }
+}
+
